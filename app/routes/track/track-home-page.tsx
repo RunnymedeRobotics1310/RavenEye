83e182022908dda41b85e37bde45a43aa2b39498
Tony Field <tony@kingsway.digital>
2026-01-03 09:10:03 -0500

    doc: Update doc to include architecture details about login flow, ui priniciples, storage principles.

--- app/routes/track/track-home-page.tsx
+++ app/routes/track/track-home-page.tsx
@@ -3,6 +3,19 @@ const TrackHomePage = () => {
     <section>
       <h1>Track</h1>
       <p>Hello world from Track Home</p>
+      <ul>
+        <li>user selects Strat area</li>
+        <li>user selects match/drill</li>
+        <li>user selects team</li>
+        <li>
+          user goes to score tracking screen
+          <br />
+          <ul>
+            <li>this can look like a game map or not</li>
+            <li>buttons denoting what can be tracked are shown</li>
+          </ul>
+        </li>
+      </ul>
     </section>
   );
 };
