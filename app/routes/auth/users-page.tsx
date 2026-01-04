743c578c6c2a7f90878181cc274f9aca145b7f9a
Tony Field <tony@kingsway.digital>
2026-01-04 10:24:39 -0500

    feat: Display user list

--- app/routes/auth/users-page.tsx
+++ app/routes/auth/users-page.tsx
@@ -1,8 +1,53 @@
+import RequireLogin from "~/common/auth/RequireLogin.tsx";
+import { useUserList } from "~/common/storage/ravenbrain.ts";
+import Spinner from "~/common/Spinner.tsx";
+import ErrorMessage from "~/common/ErrorMessage.tsx";
+
+const List = () => {
+  const { data, loading, error } = useUserList();
+  if (loading) return <Spinner />;
+  if (error)
+    return (
+      <ErrorMessage title={"Error loading user list"}>{error}</ErrorMessage>
+    );
+
+  return (
+    <section className={"usersAdmin"}>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Login</th>
+            <th>Display Name</th>
+            <th>Enabled</th>
+            <th>Forgot Password</th>
+            <th>Roles</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.login}</td>
+              <td>{user.displayName}</td>
+              <td>{user.enabled ? "Yes" : "No"}</td>
+              <td>{user.forgotPassword ? "Yes" : "No"}</td>
+              <td>{user.roles.join(", ")}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+};
 const UsersPage = () => {
   return (
     <main>
       <h1>Manage Users</h1>
-      <p>Hello world from users</p>;
+      <p>Administrator tool to manage users (login required)</p>
+      <RequireLogin showForm={false}>
+        <List />
+      </RequireLogin>
     </main>
   );
 };
