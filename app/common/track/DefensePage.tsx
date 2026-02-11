48bd52070639d8641f67fe22d1381bfaa5781077
LeiaRain <raindropp028@gmail.com>
2026-02-11 17:09:37 -0500

    More area pages

---
+++ app/common/track/DefensePage.tsx
@@ -0,0 +1,17 @@
+const DefensePage = () => {
+  return (
+    <main>
+      <div>
+        <h2>Defense</h2>
+        <p>map drawings</p>
+        <button>Start</button>
+        {"  time"}
+        <p></p>
+        <button>End</button>
+        <p>Describe defense strategy:</p>
+        <p>type box goes here</p>
+      </div>
+    </main>
+  );
+};
+export default DefensePage;
---
+++ app/common/track/EndgamePage.tsx
@@ -0,0 +1,23 @@
+const EndgamePage = () => {
+  return (
+    <main>
+      <div>
+        <p>map?</p>
+        <button>Start Climb</button>
+        {"  time"}
+        <p></p>
+        <button>Fail</button>
+        <p></p>
+        <button>Stop</button>
+        <p></p>
+        <button>L1</button>
+        {""}
+        <button>L2</button>
+        {""}
+        <button>L3</button>
+      </div>
+    </main>
+  );
+};
+
+export default EndgamePage;
